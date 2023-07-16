8dfeee2076d4c72f5d9c770665fe7e4b0d409f6c
Michael G Feeney <138948670+math-feeney@users.noreply.github.com>
2023-07-16 19:33:47 -0400

    Update functions.cpp
    Updated this function to include empty return value in case no matches are found

--- functions.cpp
+++ functions.cpp
@@ -1,22 +1,19 @@
-#include <iostream>
+// Functions
+
 #include <string>
 
-/*function that loops through a name_list string and 
-checks for a match to name, if there is then it prints
-that name*/
+/*The function below now returns a name that matches the name that 
+matches (if it matches)*/
 
 
-void name_match(std::string name_list, std::string name) {
+std::string name_match(std::string name_list, std::string name) {
 
     for (int i = 0; i < name_list.size(); i++) {
-
+        
         if (name_list.substr(i, name.size()) == name) {
-            std::cout << "There is a match!:\n";
-            std::cout << name_list.substr(i, name.size()) << "\n";
+            return name;
             break;
-
         }
-
     }
-
+    return "";
 }
