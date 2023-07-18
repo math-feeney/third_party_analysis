ce68cde7f25c15c4eabfb288bdd9c4eb62931232
Michael G Feeney <138948670+math-feeney@users.noreply.github.com>
2023-07-17 20:11:34 -0400

    Update analysis.cpp
    - added some error codes
    - fixed spacing, now names output are separated by semi colons
    - added input instructions
    -changed expected file names

--- analysis.cpp
+++ analysis.cpp
@@ -1,8 +1,12 @@
 /*****************************************************************
- * THIRD PARTY PRIVILEGE LOG ANALYSIS SOUCE CODE
+ * THIRD PARTY PRIVILEGE LOG ANALYSIS SOURCE CODE
  * AUTHOR: Michael Feeney
  * START DATE:  2023-07-07
  * *****************************************************************
+*/
+
+/*
+Dependencies: "libgcc_s_seh-1.dll", "libstdc++-6.dll", "libwinpthread-1.dll"
 */
 
 #include <iostream>
@@ -10,15 +14,23 @@
 #include <fstream>
 #include <vector>
 #include <algorithm>
+#include <windows.h>
 #include "analysis.hpp"
 
 int main() {
+
+
+    // set the sub folder that includes the necessary dll files
+    SetDllDirectoryA("dll"); // This doesn't seem to do what I want it to do
 
     char input;
 
     // This is a test input to begin analysis
     std::cout << "Welcome to the third party privlege analysis\n";
-    std::cout << "Run analysis? (y/n)\n";
+    std::cout << "Please ensure that the file names match the following:\n";
+    std::cout << "\nPriv log: 'priv_log.csv'\n";
+    std::cout << "Third party list: 'tp_list.csv'\n";
+    std::cout << "\nRun analysis? (y/n)\n";
     std::cin >> input;
 
     if (input != 'y') {
@@ -36,11 +48,14 @@ int main() {
     std::ifstream tp_file;
     std::string tp_name;
 
-    tp_file.open("tp_list.csv", std::ios::in);
+    tp_file.open("tp_list.csv", std::ios::in); // Name of tp_list file
 
     // Check if file is open, or return error
     if(!tp_file.is_open()) {
         std::cout << "Failed to open the third party list." << std::endl;
+        std::cout << "\nPlease ensure the third party list is named 'tp_list.csv'\n";
+        std::cout << "Enter any character to exit and try again:\n";
+        std::cin >> input;
         return 1;
     }
 
@@ -70,26 +85,29 @@ int main() {
     std::fstream out_file;
 
     // open output file and priv_log
-    out_file.open("text_output.csv", std::ios::out);
-    priv_file.open("test_log.csv", std::ios::in);
+    out_file.open("tp_output.csv", std::ios::out); // name of output file
+    priv_file.open("priv_log.csv", std::ios::in); // name of priv log file
 
     // check if priv_log is open or return error
     if(!priv_file.is_open()) {
         std::cout << "Failed to open the privilege log." << std::endl;
+        std::cout << "\nPlease ensure the third party list is named 'priv_log.csv'\n";
+        std::cout << "Enter any character to exit and try again:\n";
+        std::cin >> input;
         return 1;
     }
 
     // read each line of priv_log and check for names
     while (std::getline(priv_file, line)) {
 
-        std::string names_in_line = ";";
+        std::string names_in_line = ""; // default value that any name matches concat to
         
         // loop through the tp_list for each priv_log line 
         for (int i = 0; i < tp_list.size(); i++) {
 
             // this concatenates all tp matched for a given line in the priv_log
-            names_in_line += name_match(line, tp_list[i]) += " "; // Think about how to separate multiple matches
-            
+            names_in_line += name_match(line, tp_list[i]) += ""; // Think about how to separate multiple matches
+
         }
         
         tp_present.push_back(names_in_line);
